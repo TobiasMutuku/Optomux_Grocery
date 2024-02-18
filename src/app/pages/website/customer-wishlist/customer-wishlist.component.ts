24a90912ede25af60e3a23f6a55a1f0fb47bc0f4
TobiasMutuku <mutukutobias98@gmail.com>
2024-02-17 22:52:23 -0800

    website updates

---
+++ src/app/pages/website/customer-wishlist/customer-wishlist.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customer-wishlist',
+  standalone: true,
+  imports: [],
+  templateUrl: './customer-wishlist.component.html',
+  styleUrl: './customer-wishlist.component.css'
+})
+export class CustomerWishlistComponent {
+
+}
---
+++ src/app/pages/website/customers-orders/customers-orders.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-customers-orders',
+  standalone: true,
+  imports: [],
+  templateUrl: './customers-orders.component.html',
+  styleUrl: './customers-orders.component.css'
+})
+export class CustomersOrdersComponent {
+
+}
